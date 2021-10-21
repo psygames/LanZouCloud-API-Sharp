63dc85a8f33206d5044463e77213aa04f5b30131
psygames <799329256@qq.com>
2021-10-21 19:58:15 +0800

    code dir

---
+++ LanZouCode.cs
@@ -0,0 +1,20 @@
+namespace LanZouCloudAPI
+{
+    public enum LanZouCode
+    {
+        FAILED = -1,
+        SUCCESS = 0,
+        ID_ERROR = 1,
+        PASSWORD_ERROR = 2,
+        LACK_PASSWORD = 3,
+        ZIP_ERROR = 4,
+        MKDIR_ERROR = 5,
+        URL_INVALID = 6,
+        FILE_CANCELLED = 7,
+        PATH_ERROR = 8,
+        NETWORK_ERROR = 9,
+        CAPTCHA_ERROR = 10,
+        OFFICIAL_LIMITED = 11,
+        NOT_LOGIN = 12,
+    }
+}
